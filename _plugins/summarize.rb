456330ddda0f6c661a2c490a5dd4e08e752c9d24
Qiaoyong Zhong <solary.sh@gmail.com>
2012-07-30 13:03:14 +0800

    use liquid built in split filter since github does not support plugins

--- _plugins/summarize.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  module MyFilters
-    def summarize(str, splitstr = /<!--more-->/)
-      str.split(splitstr)[0]
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::MyFilters)
