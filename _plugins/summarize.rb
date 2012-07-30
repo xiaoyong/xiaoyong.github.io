392d3853bcf729a3bec7ccbc9ac7dfbd4eacf99f
Qiaoyong Zhong <solary.sh@gmail.com>
2012-07-30 10:57:53 +0800

    try to fix Liquid plugin issue

--- _plugins/summarize.rb
+++ _plugins/summarize.rb
@@ -1,7 +1,9 @@
 module Jekyll
-  module Filters
+  module MyFilters
     def summarize(str, splitstr = /<!--more-->/)
       str.split(splitstr)[0]
     end
   end
 end
+
+Liquid::Template.register_filter(Jekyll::MyFilters)
